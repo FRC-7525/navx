866e4a73996910f89dc9834ab4d48769ef25d377
nkalupahana <avishkank@gmail.com>
2022-11-18 16:46:02 -0600

    added turn 90 degrees

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,119 +10,130 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import javax.print.DocFlavor.STRING;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SerialPort;
 
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  public static AHRS navx = new AHRS(SerialPort.Port.kUSB1);
-  public static String ANGLE = "ANGLE";
-  public static double cAngle = 0;
-  public static Timer timer = new Timer();
+    public static AHRS navx = new AHRS(SerialPort.Port.kUSB1);
+    public static String ANGLE = "ANGLE";
+    public static double cAngle = 0;
+    public static Timer timer = new Timer();
 
-  public static Victor motor1 = new Victor(1);
-  public static Victor motor2 = new Victor(2);
-  public static Victor motor3 = new Victor(3);
-  public static Victor motor4 = new Victor(4);
+    public static WPI_VictorSPX left1 = new WPI_VictorSPX(1);
+    public static WPI_VictorSPX left2 = new WPI_VictorSPX(2);
+    public static WPI_VictorSPX right1 = new WPI_VictorSPX(3);
+    public static WPI_VictorSPX right2 = new WPI_VictorSPX(4);
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    cAngle = navx.getAngle();
-    SmartDashboard.putNumber(ANGLE, cAngle);
-  }
-  
-  
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
+    public DifferentialDrive drive = new DifferentialDrive(left1, right1);
     
-  
-    motor1.set(1);
-  
-    
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+        navx.reset();
 
-
-    /*if(cAngle < 90){
-
+        left2.follow(left1);
+        right2.follow(right1);
     }
-    else if(cAngle > 90) {
 
+    /**
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like
+     * diagnostics that you want ran during disabled, autonomous, teleoperated and
+     * test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+        cAngle = navx.getAngle();
+        SmartDashboard.putNumber(ANGLE, cAngle);
     }
-    
-    else if(cAngle == 0) {
 
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different
+     * autonomous modes using the dashboard. The sendable chooser code works with
+     * the Java
+     * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+     * chooser code and
+     * uncomment the getString line to get the auto name from the text box below the
+     * Gyro
+     *
+     * <p>
+     * You can add additional auto modes by adding additional comparisons to the
+     * switch structure
+     * below with additional strings. If using the SendableChooser make sure to add
+     * them to the
+     * chooser code above as well.
+     */
+    @Override
+    public void autonomousInit() {
     }
-    */
-  }
 
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {
+        if (navx.getAngle() <= 90)
+        {
+            drive.arcadeDrive(0, 0.5);
+        }
+        
+        if (navx.getAngle() > 90.5)
+        {
+            drive.arcadeDrive(0, -0.2);
+        }
+    }
 
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
+    /** This function is called once when teleop is enabled. */
+    @Override
+    public void teleopInit() {
+    }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
+    /** This function is called periodically during operator control. */
+    @Override
+    public void teleopPeriodic() {
+    }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+    /** This function is called once when the robot is disabled. */
+    @Override
+    public void disabledInit() {
+    }
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+    /** This function is called periodically when disabled. */
+    @Override
+    public void disabledPeriodic() {
+    }
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+    /** This function is called once when test mode is enabled. */
+    @Override
+    public void testInit() {
+    }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {
+    }
 }
